d1486a07f5708a000dd1bbeb939172f08b18b506
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-08-17 08:13:15 +0900

    add: トップページのルーティング処理を追加

---
+++ Routing/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use Response\HTTPRenderer;
+use Response\Render\HTMLRenderer;
+
+return [
+    '/' => function(): HTTPRenderer {
+        return new HTMLRenderer('top', []);
+    },
+];
---
+++ Views/layout/header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/934007e345.js" crossorigin="anonymous"></script>
+
+    <title>Email-Verification-System</title>
+</head>
+<body>
+    <!-- nav -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="/">Email-Verification-System</a>
+        </div>
+    </nav>
